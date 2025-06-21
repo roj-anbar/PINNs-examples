d89ea329a6bc4d1d732975b975e4c552c4ce8790
roj-anbar <rojin.anbar@gmail.com>
2025-06-21 10:03:22 -0400

    Created fenics version of 2d diffusion

--- 2d_diffusion_fenics.py
+++ 2d_diffusion_fenics.py
@@ -25,17 +25,7 @@ Date: June 2025
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# DOLFIN is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-#
-# First added:  2007-08-16
-# Last changed: 2012-11-12
+
 
 from dolfin import *
 
